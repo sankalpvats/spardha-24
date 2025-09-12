9440eeaac75d720355aea47e4ed58efc5626e79d
Sankalp <sankalp@Sankalps-Laptop.local>
2025-09-12 23:48:52 +0530

    spaceupdated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+ 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
